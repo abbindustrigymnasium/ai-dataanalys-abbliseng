5fcd0332a0c8ac5621e83abe76c7598bee8505e5
abbliseng <54662679+abbliseng@users.noreply.github.com>
2021-03-11 15:27:52 +0100

    workie

--- AI grunder/NN/sketch.js
+++ AI grunder/NN/sketch.js
@@ -7,7 +7,7 @@ let trainingdata = [
   { inputs: [1, 1], targets: [1] }
 ];
 let testingdata = [
-[0,0],[0,1],[1,0],[1,1]
+    [0,0],[0,1],[1,0],[1,1]
 ]
 function setup() {
   //Skapar nätverket
@@ -15,16 +15,15 @@ function setup() {
   
   //Skapar tränar det 100000 gånger
   for (let i = 0; i < 100000; i++) {
-    for (data of trainingdata) {
-      nn.train(data.inputs, data.targets);
-    }
+    data = random(trainingdata)
+    nn.train(data.inputs, data.targets);
   }
   //Testar det med testdatavärdena vi har angett ovan
   for (let i = 0; i < testingdata.length; i++) {
    answer=  nn.feedforward(testingdata[i]);
    console.log(answer);
-  activatedAnswer= activationfunction(answer);
-    document.getElementById('logger').innerText+="\n"+testingdata[i][0]+" och "+testingdata[i][1]+" ger svaret "+ answer+" som enligt aktiveringsfunktionen är "+activatedAnswer;
+//   activatedAnswer= activationfunction(answer);
+    // document.getElementById('logger').innerText+="\n"+testingdata[i][0]+" och "+testingdata[i][1]+" ger svaret "+ answer+" som enligt aktiveringsfunktionen är "+activatedAnswer;
     
   }//Samma som ovan fast manuelt
  // console.log( nn.feedforward([0,0])); 
@@ -34,12 +33,12 @@ function setup() {
 
 }
 
-function activationfunction(input) {
-  //Easy step activation function
-  if (input>0.5) {
-    return 1
-  }
-  else
-  return 0
-}
-function draw() {}
+// function activationfunction(input) {
+//   //Easy step activation function
+//   if (input>0.5) {
+//     return 1
+//   }
+//   else
+//   return 0
+// }
+// function draw() {}
