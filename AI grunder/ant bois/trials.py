d508931c8102972ccdb06410876cbf930ac5a265
abbliseng <54662679+abbliseng@users.noreply.github.com>
2021-04-18 14:44:40 +0200

    done?
    Allt finns men beter sig inte 100% som jag vill

--- AI grunder/ant bois/trials.py
+++ AI grunder/ant bois/trials.py
@@ -1,16 +1,20 @@
-import logging
-import threading
-import time
+# import logging
+# import threading
+# import time
 
-def thread_function(name):
-    print("Hewwo from "+str(name)+"!")
-    time.sleep(2)
-    print("Bahbah from "+str(name)+" 0/")
+# def thread_function(name):
+#     print("Hewwo from "+str(name)+"!")
+#     time.sleep(2)
+#     print("Bahbah from "+str(name)+" 0/")
 
-if __name__ == "__main__":
-    print("Starting this bad boi")
-    for i in range(10):
-        x = threading.Thread(target=thread_function, args=(i,))
-        x.start()
-    # print("Time to gooo")
-    print("Going! :D")
+# if __name__ == "__main__":
+#     print("Starting this bad boi")
+#     for i in range(10):
+#         x = threading.Thread(target=thread_function, args=(i,))
+#         x.start()
+#     # print("Time to gooo")
+#     print("Going! :D")
+n = 1
+
+for i in range(1, n):
+    print(i)
